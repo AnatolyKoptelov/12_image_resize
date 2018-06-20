ace757b0fe34276be67dc21b9a6bb45b2204a1f9
AnatolyKoptelov <7497490@gmail.com>
2018-06-20 12:55:59 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # Image Resizer
 
-[TODO. There will be project description]
 
+Application for resizing and converting an image. This application can change image sizes by your custom height *use -H option* , width *use -W option*  or scale an image with your scaling coefficient *use -s option*.
+You can set the output filename *use -o option* or application can set new filename, based on input filename and new sizes.
+If you set a file extention of your image other than input file extention, application will try to convert your image to the new file type.
+Application supports thees file extentions:
+['bmp', 'eps', 'gif', 'jpg', 'jpeg', 'png', 'tiff']
+And output file can be converted to PDF file type.
+
+# Quick Start
+
+Run application with your command line:
+```
+python image_resize.py test.jpg -H 150 -o my_new_image.bmp
+```
+And you can find your converted and resized image file *my_new_image.bmp*
+
+You can use *-h option* for help:
+```
+python  programm/devman/12_image_resize/image_resize.py -h
+usage: image_resize.py [-h] [-W WIDTH] [-H HEIGHT] [-s SCALE] [-o OUTPUT]
+                       fname
+
+Resize and/or convert an image. Type it location. Supported files extensions:
+['bmp', 'eps', 'gif', 'jpg', 'jpeg', 'png', 'tiff']
+
+positional arguments:
+  fname                 File path
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -W WIDTH, --width WIDTH
+                        Set a new width of image
+  -H HEIGHT, --height HEIGHT
+                        Set a new height of image
+  -s SCALE, --scale SCALE
+                        Set a scaling coefficient of image. Use s<1 for
+                        reduction
+  -o OUTPUT, --output OUTPUT
+                        Set a new filename
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,159 @@
+import os
+import sys
+import math
+import argparse
+import PIL.Image
+from os.path import dirname
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def get_args(extensions):
+    parser = argparse.ArgumentParser(
+        description='{}{}{}'.format(
+            'Resize and/or convert an image. Type it location. ',
+            'Supported files extensions: ',
+            str(extensions),
+        )
+    )
+    parser.add_argument(
+        'fname',
+        metavar='fname',
+        help='File path'
+    )
+    parser.add_argument(
+        '-W',
+        '--width',
+        type=int,
+        help='Set a new width of image'
+    )
+    parser.add_argument(
+        '-H',
+        '--height',
+        type=int,
+        help='Set a new height of image',
+    )
+    parser.add_argument(
+        '-s',
+        '--scale',
+        type=float,
+        help='{} {}'.format(
+            'Set a scaling coefficient of image.',
+            'Use s<1 for reduction'
+        )
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        help='Set a new filename',
+    )
+    return parser.parse_args()
+
+
+def get_file_extension(fname):
+    return fname.split('.')[-1].lower()
+
+
+def check_args(args, extensions, get_file_extension):
+    if not os.path.isfile(args.fname):
+        return 1, '{} {} {}'.format(
+            'Image file',
+            args.fname,
+            'is not found',
+        )
+    if get_file_extension(args.fname) not in extensions:
+        return 2, '.{} {}'.format(
+            get_file_extension(args.fname),
+            'files are not supported',
+        )
+    if args.scale and (args.width or args.height):
+        return 3, '{}{}\n{}'.format(
+            'Cannot to use both resize methods: ',
+            'scaling and resizing by side sizes',
+            'Try to use one of them.',
+        )
+    if args.output:
+        extensions.append('pdf',)
+        if get_file_extension(args.output) not in extensions:
+            return 4, '.{} {}'.format(
+            get_file_extension(args.output),
+            'files are not supported',
+            )
+        out_path = dirname(args.output) or dirname(args.fname) or os.getcwd()
+        if not os.access(out_path, os.W_OK):
+            return 5, '{} {}'.format(
+                'Your output path is incorrect or',
+                'permissions denied.',
+            )
+    return 0, 'Args are correct'
+
+
+def get_new_sizes(height, width, result_height, result_width, scale):
+    if scale:
+        return math.ceil(height*scale), math.ceil(width*scale)
+    if result_height and result_width:
+        return result_height, result_width
+    elif result_height:
+        return result_height, math.ceil(width*result_height/height)
+    else:
+        return math.ceil(height*result_width/width), result_width
+
+
+def get_result_fname(input_fname, output_fname, result_height, result_width ):
+    return output_fname or '{}__{}.{}'.format(
+        input_fname[:input_fname.rfind('.')],
+        'x'.join(map(str, (result_height, result_width))),
+        input_fname.split('.')[-1],
+    )
 
 
 if __name__ == '__main__':
-    pass
+    extensions = [
+        'bmp',
+        'eps',
+        'gif',
+        'jpg',
+        'jpeg',
+        'png',
+        'tiff',
+    ]
+    args = get_args(extensions)
+    exit_code, exit_text = check_args(
+        args,
+        extensions,
+        get_file_extension,
+    )
+    if exit_code:
+        sys.exit(exit_text)
+    image = PIL.Image.open(args.fname)
+    height, width = image.size
+    if args.height and args.width and height/args.height != width/args.width:
+        print('{} {}'.format(
+            'Warning! The source and result proportions of the sides',
+            'of the image do not match.',
+        ))
+        confirmation = input('Are you sure? [y/N]')
+        if confirmation != 'y':
+            sys.exit('Converting cancelled by user.')
+    result_height, result_width = get_new_sizes(
+        height=height,
+        width=width,
+        result_height=args.height,
+        result_width=args.width,
+        scale=args.scale,
+    )
+    result_fname = get_result_fname(
+        input_fname=args.fname,
+        output_fname=args.output,
+        result_height=result_height,
+        result_width=result_width,
+    )
+    image = PIL.Image.open(args.fname)
+    try:
+        image.resize((result_width, result_height)).save(result_fname)
+    except (TypeError, OSError):
+        print('{} from {} to {}\n{}'.format(
+            'Failed to convert',
+            get_file_extension(args.fname),
+            get_file_extension(args.output),
+            'Try to convert the image to a different format before!'
+        ))
